3ac23404ed91b5dc54330c9ea2d662dd12e02438
Amar <shreamar@isu.edu>
2020-04-16 19:21:42 -0600

    Admin panel authentication working

--- resources/views/layout/adminlayout/admin_sidebar.blade.php
+++ resources/views/layout/adminlayout/admin_sidebar.blade.php
@@ -1,7 +1,7 @@
 <!--sidebar-menu-->
 <div id="sidebar"><a href="{{url('/admin')}}" class="visible-phone"><i class="icon icon-home"></i> Dashboard</a>
     <ul>
-        <li><a href="{{url('/admin/dashboard')}}"><i class="icon icon-home"></i> <span>Dashboard</span></a> </li>
+        <li><a href="{{url('/dashboard')}}"><i class="icon icon-home"></i> <span>Dashboard</span></a> </li>
 
 
         <li class="submenu "> <a href="#"><i class="icon icon-th-list"></i> <span>Home</span></a>
--- routes/web.php
+++ routes/web.php
@@ -13,52 +13,16 @@
 
 //login
 Route::get('login','AdminController@index');
-Route::post('logincheck','AdminController@loginCheck');
+Route::post('adminpanel','AdminController@loginCheck');
 
 Route::get('/', 'HomeController@index');
 Route::get("/front", function(){
     return view('front');
 });
 
-Route::get('/papers/{file}', 'DownloadController@download');
-
-Route::get('file-upload', 'SubmissionController@fileUpload')->name('file.upload');
-Route::post('file-upload', 'SubmissionController@fileUploadPost')->name('file.upload.post');
-
-Route::get('file-upload', 'SubmissionController@fileUpload')->name('file.upload');
-Route::post('file-upload', 'SubmissionController@fileUploadPost')->name('file.upload.post');
-Route::get('home-create', 'HomeController@create')->name('home.create');
-Route::post('home-create', 'HomeController@store')->name('home.create.post');
 Route::get('schedule', 'ScheduleController@index')->name('Schedule.index');
-Route::get('/schedule/deleteItem/{id}','ScheduleController@destroy');
 Route::get('/Ticket', 'TicketController@index');
-Route::get('Ticket-create', 'TicketController@create')->name('Ticket.create');
-Route::post('Ticket-create', 'TicketController@store')->name('Ticket.create.post');
 Route::get('/Sponsor', 'SponsorController@index');
-Route::get('sponsor-create', 'SponsorController@create')->name('Sponsor.create');
-Route::post('sponsor-upload', 'SponsorController@store')->name('Sponsor.create.post');
-Route::get('/sponsor/deleteItem/{id}','SponsorController@destroy');
-
-//Template
-Route::resources(['template' => 'TemplateController',]);
-Route::post('/template/applyTemplate/{id}','TemplateController@applyTemplate');
-
-Route::get('/admin/dashboard', 'AdminController@dashboard');
-Route::get('/admin/Home/List', 'HomeController@list');
-Route::get('/admin/schedule/List', 'ScheduleController@list');
-Route::get('/admin/home-edit/edit/{id}','HomeController@edit')->name('home.edit');
-Route::put('/admin/home-edit/update/{id}','HomeController@update');
-Route::get('schedule-create', 'ScheduleController@create')->name('schedule.create');
-Route::post('schedule-create', 'ScheduleController@store')->name('schedule.create.post');
-Route::get('/admin/Ticket/List', 'TicketController@list');
-Route::get('/admin/schedule-edit/edit/{id}','ScheduleController@edit')->name('schedule.edit');
-Route::put('/admin/schedule-edit/update/{id}','ScheduleController@update');
-Route::get('/Ticket-edit/edit/{id}','TicketController@edit')->name('ticket.edit');
-Route::put('/Ticket-edit/update/{id}','TicketController@update');
-Route::get('/admin/sponsor/List', 'SponsorController@list');
-Route::get('/sponsor-edit/edit/{id}','SponsorController@edit')->name('sponsor.edit');
-Route::put('/sponsor-edit/update/{id}','SponsorController@update');
-Route::get('/admin/Paper/List', 'DownloadController@list');
 
 Route::get('/forgot_password','Auth\ForgotPasswordController@forgot');
 Route::post('/forgot_password','Auth\ForgotPasswordController@password');
@@ -67,3 +31,47 @@ Route::get('/passwords/reset/{token}','Auth\ResetPasswordController@showResetFor
 Route::post('passwords/reset','Auth\ResetPasswordController@reset');
 
 
+Route::group(['middleware'=>['auth','admin']], function (){
+    //template
+    Route::resources(['template' => 'TemplateController',]);
+    Route::post('/template/applyTemplate/{id}','TemplateController@applyTemplate');
+
+    Route::get('/dashboard', 'AdminController@dashboard');
+    Route::get('/admin/Home/List', 'HomeController@list');
+    Route::get('/admin/schedule/List', 'ScheduleController@list');
+    Route::get('/admin/home-edit/edit/{id}','HomeController@edit')->name('home.edit');
+    Route::put('/admin/home-edit/update/{id}','HomeController@update');
+    Route::get('schedule-create', 'ScheduleController@create')->name('schedule.create');
+    Route::post('schedule-create', 'ScheduleController@store')->name('schedule.create.post');
+    Route::get('/admin/Ticket/List', 'TicketController@list');
+    Route::get('/admin/schedule-edit/edit/{id}','ScheduleController@edit')->name('schedule.edit');
+    Route::put('/admin/schedule-edit/update/{id}','ScheduleController@update');
+    Route::get('/Ticket-edit/edit/{id}','TicketController@edit')->name('ticket.edit');
+    Route::put('/Ticket-edit/update/{id}','TicketController@update');
+    Route::get('/admin/sponsor/List', 'SponsorController@list');
+    Route::get('/sponsor-edit/edit/{id}','SponsorController@edit')->name('sponsor.edit');
+    Route::put('/sponsor-edit/update/{id}','SponsorController@update');
+    Route::get('/admin/Paper/List', 'DownloadController@list');
+
+    Route::get('/papers/{file}', 'DownloadController@download');
+
+    Route::get('file-upload', 'SubmissionController@fileUpload')->name('file.upload');
+    Route::post('file-upload', 'SubmissionController@fileUploadPost')->name('file.upload.post');
+
+    Route::get('file-upload', 'SubmissionController@fileUpload')->name('file.upload');
+    Route::post('file-upload', 'SubmissionController@fileUploadPost')->name('file.upload.post');
+    Route::get('home-create', 'HomeController@create')->name('home.create');
+    Route::post('home-create', 'HomeController@store')->name('home.create.post');
+
+    Route::get('/schedule/deleteItem/{id}','ScheduleController@destroy');
+
+    Route::get('Ticket-create', 'TicketController@create')->name('Ticket.create');
+    Route::post('Ticket-create', 'TicketController@store')->name('Ticket.create.post');
+
+    Route::get('sponsor-create', 'SponsorController@create')->name('Sponsor.create');
+    Route::post('sponsor-upload', 'SponsorController@store')->name('Sponsor.create.post');
+    Route::get('/sponsor/deleteItem/{id}','SponsorController@destroy');
+
+    //Log out
+    Route::get('logout', 'Auth\LoginController@logout')->name('logout');;
+});
